110919fff22f4bd4ef40805415914ff3b472ddd0
Carlos Juarez <carlosgj94@gmail.com>
2024-07-26 16:59:51 +0200

    test: trying to set up the rust testing environment

--- methods/src/lib.rs
+++ methods/src/lib.rs
@@ -17,9 +17,13 @@ include!(concat!(env!("OUT_DIR"), "/methods.rs"));
 
 #[cfg(test)]
 mod tests {
-    use alloy_primitives::U256;
+    use alloy_primitives::{Address, U256};
     use alloy_sol_types::SolValue;
-    use risc0_zkvm::{default_executor, ExecutorEnv};
+    use risc0_steel::{
+        config::ETH_SEPOLIA_CHAIN_SPEC,
+        ethereum::{EthEvmEnv, EthEvmInput},
+    };
+    use risc0_zkvm::{default_executor, guest::env, ExecutorEnv};
 
     #[test]
     fn proves_even_number() {
@@ -38,16 +42,29 @@ mod tests {
     }
 
     #[test]
-    #[should_panic(expected = "number is not even")]
-    fn rejects_odd_number() {
-        let odd_number = U256::from(75);
+    fn proves_voting() {
+        let evm_input: EthEvmInput;
+        let example_signature = [27, 0x8f, 0x7f, 0x3e];
+        let voter = Address::parse_checksummed("0xB0b", None).unwrap();
+        let dao = Address::parse_checksummed("0xDA0", None).unwrap();
+        let proposal_id: U256 = U256::from(0x0);
+        let direction = [1];
+        let balance: U256 = U256::from(0x0);
+        let token_contract =
+            Address::parse_checksummed("0x67a53a2b9984AF64A2e27b1582bC72406a2317c3", None).unwrap();
 
         let env = ExecutorEnv::builder()
-            .write_slice(&odd_number.abi_encode())
+            .write_slice(&evm_input)
+            .write_slice(&example_signature.abi_encode())
+            .write_slice(&voter.abi_encode())
+            .write_slice(&dao.abi_encode())
+            .write_slice(&proposal_id.abi_encode())
+            .write_slice(&direction)
+            .write_slice(&balance.abi_encode())
+            .write_slice(&token_contract.abi_encode())
             .build()
             .unwrap();
 
-        // NOTE: Use the executor to run tests without proving.
-        default_executor().execute(env, super::IS_EVEN_ELF).unwrap();
+        default_executor().execute(env, super::VOTE_ELF).unwrap();
     }
 }
